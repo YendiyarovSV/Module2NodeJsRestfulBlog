8f26fea92814ebb3ebeb9fee8c063d7e54114136
Yendiyarov Sergei <megoplay20@gmail.com>
2017-12-27 21:16:28 +0600

    Make identation

--- tests/testRequests.js
+++ tests/testRequests.js
@@ -3,7 +3,7 @@ var app = require('../server.js');
 
 //add test data
 const testObjectsCount = 5;
-for(var i=0;i<=testObjectsCount-1;i++){
+for (var i = 0; i <= testObjectsCount - 1; i++) {
   request(app)
     .post('/posts')
     .send({
@@ -116,79 +116,75 @@ request(app)
   });
 
 
-  //success get
-  request(app)
-    .get('/posts/0/comments')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end(function(err, res) {
-      if (err) throw err;
-    });
+//success get
+request(app)
+  .get('/posts/0/comments')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-    //success post a comment
-    request(app)
-      .post('/posts/0/comments')
-      .send({
-          author: "testAuthor",
-          text: "Hello test world!"
-        }
-      )
-      .expect(201)
-      .end(function(err, res) {
-        if (err) throw err;
-      });
+//success post a comment
+request(app)
+  .post('/posts/0/comments')
+  .send({
+    author: "testAuthor",
+    text: "Hello test world!"
+  })
+  .expect(201)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-      //bad request post a comment
-      request(app)
-        .post('/posts/0/comments')
-        .send({
-            author: "",
-            text: "Hello test world!"
-          }
-        )
-        .expect(400)
-        .end(function(err, res) {
-          if (err) throw err;
-        });
+//bad request post a comment
+request(app)
+  .post('/posts/0/comments')
+  .send({
+    author: "",
+    text: "Hello test world!"
+  })
+  .expect(400)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-      //success put a comment
-      request(app)
-        .put('/posts/1/comments/0')
-        .send({
-            author: "testAuthorNew",
-            text: "Hello test world!"
-          }
-        )
-        .expect(200)
-        .end(function(err, res) {
-          if (err) throw err;
-        });
+//success put a comment
+request(app)
+  .put('/posts/1/comments/0')
+  .send({
+    author: "testAuthorNew",
+    text: "Hello test world!"
+  })
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-        //not found put a comment
-        request(app)
-          .put('/posts/0/comments/30')
-          .send({
-              author: "testAuthorNew",
-              text: "Hello test world!"
-            }
-          )
-          .expect(404)
-          .end(function(err, res) {
-            if (err) throw err;
-          });
+//not found put a comment
+request(app)
+  .put('/posts/0/comments/30')
+  .send({
+    author: "testAuthorNew",
+    text: "Hello test world!"
+  })
+  .expect(404)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-  //success remove comment
-  request(app)
-    .delete('/posts/0/comments/0')
-    .expect(204)
-    .end(function(err, res) {
-      if (err) throw err;
-    });
+//success remove comment
+request(app)
+  .delete('/posts/0/comments/0')
+  .expect(204)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
 
-  //not found remove comment
-  request(app)
-    .delete('/posts/0/comments/30')
-    .expect(404)
-    .end(function(err, res) {
-      if (err) throw err;
-    });
+//not found remove comment
+request(app)
+  .delete('/posts/0/comments/30')
+  .expect(404)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
